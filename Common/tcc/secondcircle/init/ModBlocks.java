2b5d5f5b92691cfda8f56ba63c9868d2549468d9
thecountchuckula <rainsdaddy@gmail.com>
2014-10-21 16:16:27 -0700

    messed up the commit

--- Common/tcc/secondcircle/init/ModBlocks.java
+++ Common/tcc/secondcircle/init/ModBlocks.java
@@ -1,18 +1,21 @@
 package com.tcc.secondcircle.init;
 
-import com.tcc.secondcircle.block.BlockPortal;
-import com.tcc.secondcircle.block.BlockSC;
+import com.tcc.secondcircle.block.*;
 import com.tcc.secondcircle.reference.Names;
 import com.tcc.secondcircle.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks
 {
     public static final BlockSC portal = new BlockPortal();
+    public static final Block fire = new BlockFire();
 
     public static void init()
     {
         GameRegistry.registerBlock(portal, Names.Blocks.PORTAL);
+        GameRegistry.registerBlock(fire, Names.Blocks.FIRE);
     }
 }
