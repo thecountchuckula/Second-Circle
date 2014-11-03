98d21b0df76e0bb1751691545797f81033865783
thecountchuckula <rainsdaddy@gmail.com>
2014-11-03 12:46:59 -0800

    Bat Killer
    Modified the vat kill achievement. instead of 1000 bats, it is now 1
    with an arrow.
    I didn't want to figure out how to store the bat kills in a player's NBT
    data.

--- Common/tcc/secondcircle/event/BatKillEvent.java
+++ Common/tcc/secondcircle/event/BatKillEvent.java
@@ -1,18 +1,17 @@
 package com.tcc.secondcircle.event;
 
 import com.tcc.secondcircle.handler.AchievementHandler;
-import com.tcc.secondcircle.utility.LogHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.passive.EntityBat;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.init.Items;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
-public class BatKillEvent {
-    public int batkill = 1001;
-	@SubscribeEvent
+public class BatKillEvent{
+
+
+    @SubscribeEvent
 	public void onBatKill(LivingDropsEvent event) {
 		// algorithm by Pahimar
 		event.drops.clear();
@@ -20,12 +19,8 @@ public class BatKillEvent {
 			EntityPlayer player = (EntityPlayer) event.source.getEntity();
 
 			if (event.entityLiving instanceof EntityBat) {
-                batkill = batkill -1;
-                LogHelper.info((batkill - 1) + "/1000 Bats Left to Kill");
-                if(batkill == 1)
-                {
-                    player.addStat(AchievementHandler.secondCircleBat, batkill);
-                }
+
+                
 				if (event.lootingLevel == 0) {
 					int gold = event.entityLiving.worldObj.rand.nextInt(6);
 
@@ -63,6 +58,7 @@ public class BatKillEvent {
 
 					EntityPlayer player = (EntityPlayer) event.source.getEntity();
 					if (event.entityLiving instanceof EntityBat) {
+                        player.addStat(AchievementHandler.secondCircleBat, 1);
 						if (event.lootingLevel == 0) {
 							int gold = event.entityLiving.worldObj.rand
 									.nextInt(6);
